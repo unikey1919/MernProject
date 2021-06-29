e59e71e833569c7060e15c2359669719b32271de
Nguyễn Đăng Thắng <56484113+unikey1919@users.noreply.github.com>
2021-06-29 21:33:17 +0700

    Initialize UI + API

---
+++ Memories_project/server/routes/posts.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { getPosts, createPosts} from '../controllers/posts.js'
+
+const router = express.Router();
+
+// http://localhost:5000/posts
+router.get('/', getPosts);
+router.get('/', createPosts);
+
+export default router;
